d1ae0035c558614aa1b4a8a2baba98df7a3fa627
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-01-30 11:33:34 +0500

    Create Handlurls.go

---
+++ GO/Handlurls.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://loc.dev:3000/learn?coursename=reactjs&paymentid=ghhj456fgh"
+
+func main() {
+	fmt.Println("Welcome to handle Urls")
+	fmt.Println(myurl)
+
+	// Parsing
+	result, err := url.Parse(myurl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Println("The query params are:", qparams)
+
+	fmt.Println("The query params are:", qparams["coursename"])
+
+	for key, values := range qparams {
+		fmt.Printf("Key: %s, Values: %v\n", key, values)
+	}
+}
