2812a9dd3d8b7895d74159c9d648d4a32c00c352
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-02-03 11:58:32 +0500

    Update createApi.go

--- GO/createApi.go
+++ GO/createApi.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -27,16 +30,15 @@ var courses []Course
 //middleware helper file
 
 func (c *Course) isEmmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
 	fmt.Println("start making a Api in go.")
-
 }
 
 //controllers -fie
-
 //serveHome route
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
@@ -64,12 +66,81 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	for _, course := range courses {
 
-		if course.CourseId == params["id"]{
+		if course.CourseId == params["id"] {
 			json.NewEncoder(w).Encode(course)
-			return 
+			return
 		}
 	}
 
 	json.NewEncoder(w).Encode("No course found.")
+	return
+
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("create one courses")
+	w.Header().Set("Content-Type", "application/json")
+
+	//what if : body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("please send some data.")
+	}
+
+	var course Course
+
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.isEmmpty() {
+		json.NewEncoder(w).Encode("no data inside json.")
+	}
+
+	// Create a new random generator
+	// randGen := rand.New(rand.NewSource(time.Now().UnixNano()))
+	rand.NewSource(time.Now().UnixNano())
+
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+
+	// Use randGen for generating random numbers
+	// course.CourseId = fmt.Sprintf("%d", randGen.Intn(1000000))
+
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//what if : body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("please send some data.")
+	}
+
+	//get and grab id from req
+	params := mux.Vars(r)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] { // Use '==' for comparison
+			courses = append(courses[:index], courses[index+1:]...)
+
+			// fmt.Println()
+
+			var course Course
+
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			// if course.isEmmpty() {
+			// 	json.NewEncoder(w).Encode("no data inside json.")
+			// }
+
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+
+		}
+	}
+
+	//Todo: send a response when id is not found
 
 }
