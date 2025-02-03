bff0fea6023c07e817329f7c147cbdc13189bcb1
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-02-03 12:18:07 +0500

    Update createApi.go

--- GO/createApi.go
+++ GO/createApi.go
@@ -144,3 +144,20 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 	//Todo: send a response when id is not found
 
 }
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("delete one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	// loop , id , remove (index, index+1)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+
+	}
+}
