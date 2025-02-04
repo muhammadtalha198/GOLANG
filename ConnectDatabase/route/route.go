7d3c5d96ed2dd7bdf1865b6e6f7f81bd19004ba8
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-02-04 11:57:41 +0500

    Update route.go

--- ConnectDatabase/route/route.go
+++ ConnectDatabase/route/route.go
@@ -1,1 +1,21 @@
+package router
+
+import (
+	"connectDatabase/controller"
+
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkedAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("PUT")
+	router.HandleFunc("/api/deleteAllmovies", controller.DeleteAllMovie).Methods("DELETE")
+
+	return router
+
+}
 
