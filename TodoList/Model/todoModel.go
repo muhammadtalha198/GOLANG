548ac6aa34f0513e2b32cfc102357d057b85c23b
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-02-06 13:18:25 +0500

    Create todoModel.go

---
+++ TodoList/Model/todoModel.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Todo struct {
+	Id        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Task      string             `json:"task,omitempty" bson:"task,omitempty"`
+	Completed bool               `json:"completed" bson:"completed"`
+	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+}
