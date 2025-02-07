f8a239a486b21c17ed3265814377b914ac19c7c5
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-02-07 17:33:57 +0500

    Update main.go

--- TodoList/main.go
+++ TodoList/main.go
@@ -1,1 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	route "github.com/muhammadtalha198/todaapp/Route"
+)
+
+func main() {
+
+	fmt.Println("Go connect with mongo db")
+
+	// Initialize the router
+	r := route.InitializeRouter()
+
+	fmt.Println("Server is starting .....")
+
+	err := http.ListenAndServe(":8000", r)
+	if err != nil {
+		log.Fatal("Server failed to start: ", err)
+	}
+}
 
