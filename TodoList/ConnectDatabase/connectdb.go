098f12ebded47e7a766e54b29dbe731dcb8e9ff5
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-02-07 18:42:43 +0500

    Update connectdb.go

--- TodoList/ConnectDatabase/connectdb.go
+++ TodoList/ConnectDatabase/connectdb.go
@@ -9,10 +9,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const connectionString = "mongodb+srv://admin:12345@cluster0.zqufr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-const dbName = "Todo_Database"
-const colName = "Todo_Collection"
+const (
+	connectionString = "mongodb+srv://admin:12345@cluster0.zqufr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+	dbName           = "Todo_Database"
+	colName          = "Todo_Collection"
+)
 
 var collection *mongo.Collection
 
@@ -28,9 +29,37 @@ func init() {
 	}
 	fmt.Println("Mongo db connected successfully.")
 
+	err = _client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
 	collection = _client.Database(dbName).Collection(colName)
 
 	//collection instance
 	fmt.Println("connection instance is ready!")
 
+}
+
+// ConnectDB initializes the MongoDB client and returns a collection
+func ConnectDB() *mongo.Collection {
+
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
+	collection := client.Database(dbName).Collection(colName)
+	return collection
 }
